e477a103c7ac5aeec18931783a6ff1553549e39c
Hives <communty.hivemind@gmail.com>
2018-10-04 15:14:41 +0100

    Initial commit

---
+++ example.py
@@ -0,0 +1,43 @@
+from paulsInvoices import Invoice, InvoiceTable
+from datetime import date
+
+hourly_rate = 66
+
+output_table = InvoiceTable()
+
+output_table.addHourlyItem(["Clean the toilet", 0.16, hourly_rate])
+output_table.addHourlyItem(["Scrub the floor", 9.40, hourly_rate])
+output_table.addHourlyItem(["Take out the rubbish", 19.79, hourly_rate])
+output_table.addSubtotal("Cleaning subtotal")
+output_table.addFixedItem(["This is a one off task with no hourly rate", 101.20])
+output_table.addHourlyItem(["Darn your socks", 20, hourly_rate])
+
+inv = Invoice()
+
+# your personal details (use <br> for multiple lines):
+inv.name = "Joe Bloggs"
+inv.address = """56 Avenue Street<br>
+				 Townsville<br>
+				 123 ABC"""
+inv.phone = "01223 334444"
+inv.email = "joe.bloggs@joebloggs.com"
+
+# invoice details
+inv.invoice_id ="INVOICE01"
+inv.period = "September 2018" # the period to which the invoice refers - optional
+inv.date = date(2018, 10, 4) # year, month, day
+
+# client details
+inv.client = "Janet Colgate"
+inv.client_address = """99 Boulevard Road<br>
+						Cityborough<br>
+						789 XYZ"""
+
+# how you accept payment
+inv.payment_details = """Please make cheques payable to Joe Bloggs,<br>
+						 or bank transfer to Joe Bloggs, Frank's Bank, 12345678 12-34-56"""
+
+# the actual details
+inv.invoice_table = output_table
+
+inv.render()
---
+++ paulsInvoices.py
@@ -0,0 +1,166 @@
+from datetime import date
+
+class Invoice:
+    """Paul's invoices"""
+    template = "invoice-template.html"
+
+    def __init__(self):
+        self.name = False
+        self.address = False
+        self.phone = False
+        self.email = False
+        self.invoice_id = False
+        self.period = False
+        self.date = False
+        self.client = False
+        self.client_address = False
+        self.payment_details = False
+        self.invoice_table = False
+
+    def render(self):
+        if self.invoice_id:
+            output_file = self.invoice_id + ".html"
+        else:
+            output_file = "output.html"
+
+        with open(self.template) as t, open(output_file, "w") as o:
+            for line in t:
+
+                # mandatory items in simple string format
+                for item in ['name', 'address', 'phone', 'email', 'invoice_id', 'client', 'payment_details']:
+                    searchstring = "{+%s+}" % item
+                    if searchstring in line:
+                        if getattr(self, item):
+                            line = line.replace(searchstring, getattr(self, item))
+                        else:
+                            line = line.replace(searchstring, "<span class='whoops'>No " + item +"?!</span>")
+
+                # optional items - deletes whole line if not entered
+                for item in ['period', 'client_address']:
+                    searchstring = "{+%s+}" % item
+                    if searchstring in line:
+                        if getattr(self, item):
+                            line = line.replace(searchstring, getattr(self, item))
+                        else:
+                            line = ""
+
+                # items with formatting requirements
+                if "{+date+}" in line:
+                    if self.date:
+                        line = line.replace("{+date+}", format_date(self.date))
+                    else:
+                        line = line.replace("{+date+}", "<span class='whoops'>No date?!</span>")
+
+                if "{+invoice_table+}" in line:
+                    if self.invoice_table:
+                        line = line.replace("{+invoice_table+}", self.invoice_table.render())
+                    else:
+                        line = line.replace(searchstring, "<span class='whoops'>No invoice_table?!</span>")
+
+
+                o.write(line)
+
+class InvoiceTable:
+    """Paul's output table class with hourly calculations"""
+
+    def __init__ (self):
+        self.header = []
+        self.rows = []
+        self.columns = 3
+
+    def addHeader(self, fields):
+        self.header.append(fields)
+
+    def addHourlyItem(self, fields):
+        self.rows.append(["hourly", fields])
+
+    def addFixedItem(self, fields):
+        self.rows.append(["fixed", fields])
+
+    def addSubtotal(self, text):
+        self.rows.append(["subtotal", text])
+
+    def render(self):
+        output = "<table>"
+        count_hours = 0
+        count_money = 0
+        sub_count_hours = 0
+        sub_count_money = 0
+
+        # table header
+        output += "<thead><tr>"
+        output += "<th class='task'>Task</th>"
+        output += "<th class='hours'>Hours</th>"
+        output += "<th class='money'>Amount</th>"
+        output += "</tr></thead>"
+        
+        output += "<tbody>"
+
+        for row in self.rows:
+            type = row[0]
+            
+            if type == "hourly":
+                fields = row[1]
+
+                description = fields [0]
+                hours = fields[1]
+                money = fields[1]*fields[2]
+
+                count_hours += hours
+                count_money += money
+                sub_count_hours += hours
+                sub_count_money += money
+               
+                output += "<tr>"
+                output += "<td class='task'>" + description + "</td>"
+                output += "<td class='hours'>" + format_hours(hours) + "</td>"
+                output += "<td class='money'>" + format_money(money) + "</td>"
+                output += "<tr>"
+
+            if type == "fixed":
+                fields = row[1]
+
+                description = fields[0]
+                money = fields[1]
+
+                count_money += money
+                sub_count_money += money
+               
+                output += "<tr>"
+                output += "<td class='task'>" + description + "</td>"
+                output += "<td></td>"
+                output += "<td class='money'>" + format_money(money) + "</td>"
+                output += "<tr>"
+
+            if type == "subtotal":
+                output += "<tr class='subtotal'>"
+                output += "<td>" + row[1] + ":</td>"
+                output += "<td class='hours'>" + format_hours(sub_count_hours) + "</td>"
+                output += "<td class='money'>" + format_money(sub_count_money) + "</td>"
+                output += "<tr>"
+
+                sub_count_hours = 0
+                sub_count_money = 0
+
+        output += "</tbody>"
+
+        output += "<tfoot>"
+        output += "<tr>"
+        output += "<td colspan=2>Balance due:</td>"
+        output += "<td class='money'>" + format_money(count_money) + "</td>"
+        output += "<tr>"
+        output += "</tfoot>"
+
+        output += "</table>"
+
+        return output
+
+def format_hours(h):
+    return '{0:.2f}'.format(h)
+
+def format_money(m):
+    return "<span class='pound-sign'>&pound;</span>" + '{0:,.2f}'.format(m)
+
+def format_date(d):
+    date_format = "%d %b %Y"
+    return d.strftime(date_format)
